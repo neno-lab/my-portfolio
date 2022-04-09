5a7a95fcac3741835835ff67f429e2d3d494d1f0
Neno Zidic <nenozidic@Igors-MacBook-Pro.local>
2022-04-09 16:40:32 +0200

    Init commit

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import LayoutDesktop from './layoutDesktop';
+import LayoutMobile from './layoutMobile';
+import React from 'react';
+import useWindowSize from '../helpers/hooks/useWindowSize';
+
+const App = () => {
+  const windowSize = useWindowSize();
+
+  if (windowSize.width >= 1025) return <LayoutDesktop />;
+  return <LayoutMobile />;
+};
+
+export default App;
