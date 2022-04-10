9c6bd0a6276c76242da22554839262f16638adc6
Neno Zidic <nenozidic@Igors-MacBook-Pro.local>
2022-04-11 01:48:25 +0200

    All pages title are now prepared

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,19 @@
 import './style.scss';
 
+import DivContent from '../../containers/DivContent';
 import Main from '../../containers/Main';
 import React from 'react';
+import { TiContacts } from 'react-icons/ti';
+import Underline from '../../components/universal/Underline';
 
 const Contact = () => (
-  <Main className='contact-main-holder' />
+  <Main className='contact-main-holder'>
+    <DivContent className='contact-title-holder'>
+      <TiContacts />
+      <h1>Contact</h1>
+      <Underline />
+    </DivContent>
+  </Main>
 );
 
 export default Contact;
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,10 +1,19 @@
 import './style.scss';
 
+import { AiOutlineFundProjectionScreen } from 'react-icons/ai';
+import DivContent from '../../containers/DivContent';
 import Main from '../../containers/Main';
 import React from 'react';
+import Underline from '../../components/universal/Underline';
 
 const Projects = () => (
-  <Main className='projects-main-holder' />
+  <Main className='projects-main-holder'>
+    <DivContent className='projects-title-holder'>
+      <AiOutlineFundProjectionScreen />
+      <h1>Projects</h1>
+      <Underline />
+    </DivContent>
+  </Main>
 );
 
 export default Projects;
